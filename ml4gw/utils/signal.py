6aa0b8b7d22ef427444d346bfb645756d1a5f1ef
Ethan Marx <emarx@mit.edu>
2023-06-02 15:29:24 -0500

    torchified time domain filter building

--- ml4gw/utils/signal.py
+++ ml4gw/utils/signal.py
@@ -22,35 +22,36 @@ def planck(N: int, nleft: int = 0, nright: int = 0):
     return w
 
 def truncate_impulse(impulse: BatchTimeSeriesTensor, ntaps: ScalarTensor):
-    trunc_start = (ntaps / 2).astype(torch.int8)
-    size = impulse.size(-1)
+    out = impulse.clone()
+    trunc_start = int(ntaps / 2)
+    size = out.size(-1)
     trunc_stop = size - trunc_start
     # TODO: implement other windowing functions
     window = torch.hann_window(ntaps)
-    impulse[:, :, 0:trunc_start] *= window[trunc_start:ntaps]
-    impulse[:, :, trunc_stop:impulse.size] *= window[0:trunc_start]
-    impulse[:,:, trunc_start:trunc_stop] = 0
-    return impulse
+    out[:, :, 0:trunc_start] *= window[trunc_start:ntaps]
+    out[:, :, trunc_stop:size] *= window[0:trunc_start]
+    out[:,:, trunc_start:trunc_stop] = 0
+    return out
 
 
 def truncate_transfer(transfer, ncorner: Optional[int] = None):
     out = transfer.clone()
-    if out.ndim == 1:
-        out = out[None, None, :]
     nsamp = out.size(-1)
     ncorner = ncorner if ncorner else 0
     out[:, :, 0:ncorner] = 0
     out[:, :, ncorner:nsamp] *= planck(nsamp-ncorner, nleft=5, nright=5)
-    print(transfer[0][0][:10])
     return out
 
 def fir_from_transfer(transfer, ntaps, ncorner=None):
-    
     # truncate and highpass the transfer function
     transfer = truncate_transfer(transfer, ncorner=ncorner)
+
     # compute and truncate the impulse response
-    impulse = torch.rfft(transfer)
+    impulse = torch.fft.irfft(transfer)
+   
     impulse = truncate_impulse(impulse, ntaps=ntaps)
+    print(impulse.shape)
     # wrap around and normalise to construct the filter
-    out = torch.roll(impulse, int(ntaps/2 - 1))[0:ntaps]
+    out = torch.roll(impulse, int(ntaps/2 - 1), dims=-1)[:,:,0:ntaps]
+
     return out
--- tests/utils/test_signal.py
+++ tests/utils/test_signal.py
@@ -9,7 +9,7 @@ import numpy as np
 def n_channels(request):
     return request.param
 
-@pytest.fixture(params=[128, 512, 1024])
+@pytest.fixture(params=[2028])
 def size(request):
     return request.param
 
@@ -23,6 +23,10 @@ def timeseries(batch_size, n_channels, size):
 
 @pytest.fixture(params=[0, 2, 4])
 def ncorner(request):
+    return request.param
+
+@pytest.fixture(params=[32, 64])
+def ntaps(request):
     return request.param
 
 @pytest.fixture(params=[0, 1, 2, 3])
@@ -40,24 +44,37 @@ def test_planck_window(size, nleft, nright):
 
 
 def test_truncate_transfer(timeseries, ncorner):
-    print(timeseries[0][0][:10])
     transfer = truncate_transfer(timeseries, ncorner)
-    print(timeseries[0][0][:10])
-    print(asfd)
+    
     timeseries = timeseries.numpy().reshape(-1, timeseries.shape[-1])
     transfer = transfer.numpy().reshape(-1, transfer.shape[-1])
 
     for tf, ts in zip(transfer, timeseries):
         gwpy_tf = gwpy_filter_functions.truncate_transfer(ts, ncorner)
-        print(tf[:10], gwpy_tf[:10], ts[:10])
         assert np.allclose(tf, gwpy_tf)
 
 
-def test_truncate_impulse():
-    pass
+def test_truncate_impulse(timeseries, ncorner):
+    transfer = truncate_impulse(timeseries, ncorner)
+    
+    timeseries = timeseries.numpy().reshape(-1, timeseries.shape[-1])
+    transfer = transfer.numpy().reshape(-1, transfer.shape[-1])
 
-def fir_from_transfer():
-    pass
+    for tf, ts in zip(transfer, timeseries):
+        gwpy_tf = gwpy_filter_functions.truncate_impulse(ts, ncorner)
+        assert np.allclose(tf, gwpy_tf)
+
+def test_fir_from_transfer(timeseries, ntaps):
+    transfer = fir_from_transfer(timeseries, ntaps, ncorner=0)
+   
+
+    timeseries = timeseries.numpy().reshape(-1, timeseries.shape[-1])
+    transfer = transfer.numpy().reshape(-1, transfer.shape[-1])
+   
+    for tf, ts in zip(transfer, timeseries):
+        gwpy_tf = gwpy_filter_functions.fir_from_transfer(ts, ntaps, ncorner=0)
+        assert np.allclose(tf, gwpy_tf)
+    
 
 
 
