4404f85e19be3e40e31d2715f1b91e56b65f6f48
William Benoit <william.benoit@dgx1.ligo-wa.caltech.edu>
2024-09-27 14:05:24 -0700

    Added spline interpolation code

--- ml4gw/transforms/__init__.py
+++ ml4gw/transforms/__init__.py
@@ -4,6 +4,10 @@ from .scaler import ChannelWiseScaler
 from .snr_rescaler import SnrRescaler
 from .spectral import SpectralDensity
 from .spectrogram import MultiResolutionSpectrogram
+<<<<<<< HEAD
 from .spline_interpolation import SplineInterpolate
+=======
+from .spline_interpolation import SplineInterpolation
+>>>>>>> e6dc497 (Added spline interpolation code)
 from .waveforms import WaveformProjector, WaveformSampler
 from .whitening import FixedWhiten, Whiten
